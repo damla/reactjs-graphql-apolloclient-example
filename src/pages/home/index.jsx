80cb295e012efeb13f6f008873029ed801e92021
Damla Koksal <me@damlakoksal.com>
2021-10-16 23:03:53 +0300

    [#3] feat: implement CRA with base components

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import Container from "../../components/container";
+import Layout from "../../components/layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Container>Home</Container>
+    </Layout>
+  );
+}
